7a90bfcd1134615eb2ea2f2b4ed2917ce1076696
Hailey Kim <haileydeveloper@gmail.com>
2024-05-27 18:21:49 +0900

    chore: set up Tanstack Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/styles/globals.css";
 
 export const metadata: Metadata = {
   title: "클래스브릿지",
   description: "클래스브릿지",
 };
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children,
@@ -14,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="/favicon/favicon.ico" type="image/x-icon" />
-      <body className="font-pretendard">{children}</body>
+      <QueryClientProvider client={queryClient}>
+        <body className="font-pretendard">{children}</body>
+      </QueryClientProvider>
     </html>
   );
 }
