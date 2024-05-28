de8ddc4071b026a2a51404a462118f38c23c5d8b
{hwayoon] <{rup_rupes@naver.com}>
2024-05-29 00:38:33 +0900

    feat: work on Header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,19 +1,30 @@
+"use client";
+
 import Link from "next/link";
+import { MagnifyingGlass } from "@/asset/icons";
 
 export default function Header() {
   return (
-    <header className="flex items-center w-256 box-border">
-      <Link href="/" className="text-xl">
-        <h1 className="pr-5 pl-5">
-          <span className="text-primary font-bold">C</span>LASS{" "}
-          <span className="text-secondary font-bold">B</span>
+    <header className="flex justify-center items-center gap-5 w-[1024px] h-20">
+      <Link href="/" className="">
+        <h1 className="flex justify-center items-center w-[150px] h-10 font-bold text-xl">
+          <span className="text-primary ">C</span>LASS{" "}
+          <span className="text-secondary">B</span>
           RIDGE
         </h1>
       </Link>
-      <button className="pr-5 pl-5">카테고리</button>
-      <button className="pr-5 pl-5">지역</button>
-      <button className="pr-7 pl-7">SEARCH BAR</button>
-      <button className="pr-5 pl-5">로그인</button>
+      <button onClick={() => {}} className="w-20 h-9">
+        카테고리
+      </button>
+      <button onClick={() => {}} className="w-20 h-9">
+        지역
+      </button>
+      <button className="w-[440px] ml-2 mr-[54px] py-[11px] pl-[14px] border border-black rounded-md">
+        <MagnifyingGlass />
+      </button>
+      <button className="w-[100px] h-10 bg-primary rounded-md text-white text-sm">
+        로그인
+      </button>
     </header>
   );
 }
