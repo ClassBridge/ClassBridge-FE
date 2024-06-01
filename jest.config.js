e6896466e906e0f0d82593872224d8682ba22dc1
Hailey Kim <haileydeveloper@gmail.com>
2024-06-01 18:29:05 +0900

    fix: jest error

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
