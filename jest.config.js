fd55753a78c5de1fdf520a2b34ed839b424508fb
Hailey Kim <haileydeveloper@gmail.com>
2024-06-01 18:49:39 +0900

    fix: too many errors, decided not to use CI check and Jest testing

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["/node_modules/", "/.next/"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-};
