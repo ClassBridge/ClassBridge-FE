9b054e8679ace92cfdad2c35f815d87c93d46e1c
Hailey Kim <haileydeveloper@gmail.com>
2024-06-03 11:49:04 +0900

    feat: update InfoForma and schema

--- src/components/form/SignUpInfoForm.tsx
+++ src/components/form/SignUpInfoForm.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useRef, useState } from "react";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { cn, formatDate, formatPhoneNumber } from "@/lib/utils";
+import { signUpInfoFormSchema } from "@/lib/formSchema";
 
 import { Input } from "@/components/ui/input";
 import {
@@ -13,28 +16,28 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-
-import { signUpInfoFormSchema } from "@/lib/formSchema";
-import { useRef, useState } from "react";
-import Image from "next/image";
 import Button from "../common/Button";
 import ProfilePicture from "../common/ProfilePicture";
-import { cn } from "@/lib/utils";
 
 type SignUpInfoFormData = z.infer<typeof signUpInfoFormSchema>;
 
 const signUpInfoFormField: {
-  value: "username" | "phone" | "picture" | "gender" | "birthdate" | "interest";
+  value:
+    | "username"
+    | "phoneNumber"
+    | "profilePictureUrl"
+    | "gender"
+    | "birthDate"
+    | "interests";
   label: string;
   required?: boolean;
 }[] = [
   { value: "username", label: "유저이름", required: true },
-  { value: "phone", label: "연락처", required: true },
-  { value: "picture", label: "프로필 사진" },
+  { value: "phoneNumber", label: "연락처", required: true },
+  { value: "profilePictureUrl", label: "프로필 사진" },
   { value: "gender", label: "성별" },
-  { value: "birthdate", label: "생년월일" },
-  { value: "interest", label: "관심사" },
+  { value: "birthDate", label: "생년월일" },
+  { value: "interests", label: "관심사" },
 ];
 
 type Gender = "male" | "female";
@@ -75,9 +78,12 @@ interface Props {
 }
 
 export default function SignUpInfoForm({ toSuccessPage }: Props) {
+  const [isUsernameChecked, setIsUsernameChecked] = useState<boolean>(false);
   const [isValidUsername, setIsValidUsername] = useState<boolean>(false);
+
   const [selectedGender, setSelectedGender] = useState<Gender>();
   const [selectedInterest, setSelectedInterest] = useState<Interest[]>([]);
+
   const [preview, setPreview] = useState<string>();
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
@@ -85,13 +91,21 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
     resolver: zodResolver(signUpInfoFormSchema),
   });
 
-  const handleButtonClick = () => {
+  const handleUsernameCheck = () => {
+    setIsUsernameChecked(true);
+    if (isUsernameChecked) {
+      setIsValidUsername(true);
+    }
+    // TODO check username validity API
+  };
+
+  const handleImageUpload = () => {
     if (fileInputRef.current) {
       fileInputRef.current.click();
     }
   };
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file) {
       const reader = new FileReader();
@@ -103,7 +117,22 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
   };
 
   const onSubmit = (data: SignUpInfoFormData) => {
-    console.log(data);
+    if (!isUsernameChecked) {
+      return alert("유저이름 중복확인을 완료해 주세요.");
+      // TODO use custom alert component
+    }
+    if (!isValidUsername) {
+      return;
+    }
+
+    const formData = {
+      ...data,
+      gender: selectedGender,
+      interests: selectedInterest,
+      profilePictureUrl: preview,
+    };
+    console.log(formData);
+
     toSuccessPage();
   };
 
@@ -116,17 +145,38 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
           name={signUpInfoFormField[0].value}
           render={({ field }) => (
             <FormItem className="text-black">
-              <FormLabel>{`${signUpInfoFormField[0].label} (필수)`}</FormLabel>
-              <FormControl>
-                <div className="flex gap-2">
+              <FormLabel>
+                <div className="flex justify-between">
+                  <span>{`${signUpInfoFormField[0].label} (필수)`}</span>
+                  {isUsernameChecked &&
+                    (isValidUsername ? (
+                      <span className="text-primary">
+                        {"*사용 가능한 유저이름입니다."}
+                      </span>
+                    ) : (
+                      <span className="text-point-like">
+                        {"*이미 있는 유저이름입니다."}
+                      </span>
+                    ))}
+                </div>
+              </FormLabel>
+              <div className="flex gap-2">
+                <FormControl>
                   <Input
+                    {...field}
+                    autoComplete="off"
                     placeholder={signUpInfoFormField[0].label}
                     className="rounded border-gray-light placeholder:text-gray modal-input"
                     value={field.value as string}
                   />
-                  <Button text="중복확인" primary type="sm" />
-                </div>
-              </FormControl>
+                </FormControl>
+                <Button
+                  text="중복확인"
+                  primary
+                  type="sm"
+                  onClick={handleUsernameCheck}
+                />
+              </div>
               <FormMessage />
             </FormItem>
           )}
@@ -141,9 +191,15 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
               <FormLabel>{`${signUpInfoFormField[1].label} (필수)`}</FormLabel>
               <FormControl>
                 <Input
+                  {...field}
+                  autoComplete="off"
                   placeholder={signUpInfoFormField[1].label}
                   className="rounded border-gray-light placeholder:text-gray modal-input"
                   value={field.value as string}
+                  onChange={(e) => {
+                    const formattedValue = formatPhoneNumber(e);
+                    field.onChange(formattedValue);
+                  }}
                 />
               </FormControl>
               <FormMessage />
@@ -155,23 +211,25 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
           key={signUpInfoFormField[2].value}
           control={form.control}
           name={signUpInfoFormField[2].value}
-          render={({ field }) => (
+          render={() => (
             <FormItem className="text-black">
               <FormLabel>{signUpInfoFormField[2].label}</FormLabel>
               <div className="flex gap-10">
                 <ProfilePicture src={preview} fallback="CB" />
-                <input
-                  type="file"
-                  accept="image/*"
-                  className="hidden"
-                  ref={fileInputRef}
-                  onChange={handleFileChange}
-                />
+                <FormControl>
+                  <Input
+                    type="file"
+                    accept="image/*"
+                    className="hidden"
+                    ref={fileInputRef}
+                    onChange={handleImageChange}
+                  />
+                </FormControl>
                 <Button
                   text="사진 업로드"
                   primary
                   type="sm"
-                  onClick={handleButtonClick}
+                  onClick={handleImageUpload}
                 />
               </div>
               <FormMessage />
@@ -192,6 +250,7 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
                     <button
                       key={gender.value}
                       value={gender.value}
+                      type="button"
                       className={cn(
                         "w-20 h-full transition duration-300",
                         i === 0
@@ -204,7 +263,9 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
                       {gender.label}
                     </button>
                   ))}
-                  <input type="hidden" name="gender" value={selectedGender} />
+                  <FormControl>
+                    <Input {...field} className="hidden" />
+                  </FormControl>
                 </div>
                 <FormMessage />
               </FormItem>
@@ -219,9 +280,14 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
                 <FormLabel>{signUpInfoFormField[4].label}</FormLabel>
                 <FormControl>
                   <Input
+                    {...field}
                     placeholder={signUpInfoFormField[4].label}
                     className="rounded border-gray-light placeholder:text-gray modal-input"
                     value={field.value as string}
+                    onChange={(e) => {
+                      const formattedValue = formatDate(e);
+                      field.onChange(formattedValue);
+                    }}
                   />
                 </FormControl>
                 <FormMessage />
@@ -238,10 +304,11 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
             <FormItem className="text-black">
               <FormLabel>{signUpInfoFormField[5].label}</FormLabel>
               <div className="grid grid-cols-5 gap-2 font-medium text-sm">
-                {interests.map((interest) => (
+                {interests.map((interest, i) => (
                   <button
-                    key={interest.value}
+                    key={i}
                     value={interest.value}
+                    type="button"
                     className={cn(
                       "h-14 rounded border border-gray-light transition duration-300",
                       selectedInterest.includes(interest.value) &&
@@ -263,7 +330,9 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
                     {interest.label}
                   </button>
                 ))}
-                <input type="hidden" name="interest" value={selectedInterest} />
+                <FormControl>
+                  <Input {...field} className="hidden" />
+                </FormControl>
               </div>
               <FormMessage />
             </FormItem>
@@ -274,7 +343,6 @@ export default function SignUpInfoForm({ toSuccessPage }: Props) {
           <button
             type="submit"
             className="py-2.5 rounded font-medium text-base text-white bg-primary"
-            onClick={toSuccessPage}
           >
             {"완료하기"}
           </button>
--- src/lib/formSchema.ts
+++ src/lib/formSchema.ts
@@ -61,19 +61,36 @@ export const signUpFormSchema = z
 
 export const signUpInfoFormSchema = z.object({
   username: z.string({ required_error: MESSAGE.required }),
-  phone: z
+  phoneNumber: z
     .string({ required_error: MESSAGE.required })
-    .regex(/^\d{10,11}$/, { message: MESSAGE.invalidPhone }),
-  picture: z
-    .instanceof(File)
-    .refine((file) => file.type.startsWith("image/"), {
-      message: MESSAGE.invalidImage,
-    })
-    .optional(),
+    .regex(/^\d{3}-\d{4}-\d{4}$/, { message: MESSAGE.invalidPhone }),
+  profilePictureUrl: z.string().optional(),
   gender: z.enum(["male", "female"]).optional(),
-  birthdate: z
+  birthDate: z
     .string()
     .regex(/^\d{4}-\d{2}-\d{2}$/, { message: MESSAGE.invalidDate })
+    .refine(
+      (value) => {
+        const parts = value.split("-");
+        const year = parseInt(parts[0]);
+        const month = parseInt(parts[1]) - 1;
+        const day = parseInt(parts[2]);
+
+        if (year < 1900 || year > new Date().getFullYear() - 1) {
+          return false;
+        }
+
+        const dateObject = new Date(year, month, day);
+        return (
+          dateObject.getFullYear() === year &&
+          dateObject.getMonth() === month &&
+          dateObject.getDate() === day
+        );
+      },
+      {
+        message: MESSAGE.invalidDate,
+      }
+    )
     .optional(),
-  interest: z.array(z.string()).optional(),
+  interests: z.array(z.string()).optional(),
 });
