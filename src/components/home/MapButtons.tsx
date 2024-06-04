3beba0365475c4a8ef9cc826a89cf697abe19364
Hailey Kim <haileydeveloper@gmail.com>
2024-06-04 17:56:19 +0900

    feat: use Geolocation, create geolocationState

--- src/components/home/MapButtons.tsx
+++ src/components/home/MapButtons.tsx
@@ -1,28 +1,55 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { geolocationState } from "@/state/geolocation";
 import { cn } from "@/lib/utils";
 
 export default function MapButtons() {
-  const [isMapOpen, setIsMapOpen] = useState(false);
-  const [isGeolocation, setIsGeolocation] = useState(false);
+  const [isMapOpen, setIsMapOpen] = useState<boolean>(false);
+  const [isGeolocation, setIsGeolocation] = useState<boolean>(false);
+  const setGeolocation = useSetRecoilState(geolocationState);
+  const [alert, setAlert] = useState<string>();
 
-  const handleToggleMap = () => {
+  const handleToggleMap = useCallback(() => {
     const map = document.getElementById("map");
     map?.classList.toggle("hidden");
 
-    setIsMapOpen(!isMapOpen);
+    setIsMapOpen((prev) => !prev);
     if (isMapOpen) {
       setIsGeolocation(false);
     }
-  };
+  }, [isMapOpen]);
 
-  const handleUseGeolocation = () => {
+  const handleUseGeolocation = useCallback(() => {
+    let error = false;
+
+    navigator.geolocation.getCurrentPosition(
+      (pos) =>
+        setGeolocation({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+          accuracy: pos.coords.accuracy,
+        }),
+      (err) => {
+        const alertMessage =
+          err.code === 1
+            ? "위치 정보 사용 동의 시 이용 가능한 서비스입니다."
+            : "위치 정보를 불러오는데 실패했습니다. 다시 시도해 주세요.";
+        setAlert(alertMessage);
+        error = true;
+      },
+    );
+
+    if (error) {
+      return;
+    }
+
     setIsGeolocation(true);
     if (!isMapOpen) {
       handleToggleMap();
     }
-  };
+  }, [handleToggleMap, isMapOpen, setGeolocation]);
 
   return (
     <div className="flex gap-5">
