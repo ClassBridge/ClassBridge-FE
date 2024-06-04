36e578aca770f86d145ce95e5a1751f201ec74ae
Hailey Kim <haileydeveloper@gmail.com>
2024-06-04 21:49:42 +0900

    feat: create alertState and update Alert component

--- src/components/common/Alert.tsx
+++ src/components/common/Alert.tsx
@@ -1,15 +1,32 @@
+"use client";
+
+import { useRecoilValue, useResetRecoilState } from "recoil";
+import { alertState } from "@/state/alert";
 import Button from "./Button";
 
 export default function Alert() {
+  const alert = useRecoilValue(alertState);
+  const closeAlert = useResetRecoilState(alertState);
+
   return (
-    <div className="fixed bottom-2/4 right-2/4 translate-x-2/4 translate-y-2/4 z-50 flex flex-col gap-4 p-4 rounded border bg-white">
-      <p
-        dangerouslySetInnerHTML={{
-          __html:
-            "이 창은 도움말, 안내 등의 메세지를 보여줍니다.<br />닫기 버튼을 눌러 닫을 수 있습니다.",
-        }}
-      />
-      <Button text="닫기" type="sm" primary className="self-end w-10" />
-    </div>
+    <>
+      {alert && (
+        <div className="fixed bottom-2/4 right-2/4 translate-x-2/4 translate-y-2/4 z-50 flex flex-col gap-4 py-4 px-5 rounded border border-gray-light bg-white">
+          <h3 className="font-bold text-lg">{"알림"}</h3>
+          <p
+            dangerouslySetInnerHTML={{
+              __html: alert,
+            }}
+          />
+          <Button
+            text="닫기"
+            type="sm"
+            primary
+            className="self-end w-12"
+            onClick={closeAlert}
+          />
+        </div>
+      )}
+    </>
   );
 }
