58060f8d0b4596c488f263d4a637677f63103850
Hailey Kim <haileydeveloper@gmail.com>
2024-06-07 16:13:32 +0900

    feat: update StarRating

--- src/components/common/StarRating.tsx
+++ src/components/common/StarRating.tsx
@@ -1,35 +1,30 @@
 import { Star, StarHalf } from "lucide-react";
+import COLORS from "@/constants/colors";
 
 interface Props {
   size: number;
-  initialValue?: number;
-  editable?: boolean;
-  onChange?: () => void;
+  value?: number;
 }
 
-export default function StarRating({
-  size,
-  initialValue = 0,
-  editable = false,
-  onChange,
-}: Props) {
+export default function StarRating({ size, value = 0 }: Props) {
   return (
     <div className="relative">
       <div className="flex gap-0.5">
         {[1, 2, 3, 4, 5].map((i) => (
-          <Star key={i} stroke="#FFC700" strokeWidth={1.5} size={size} />
+          <Star
+            key={i}
+            stroke={COLORS.point.star}
+            strokeWidth={1.5}
+            size={size}
+          />
         ))}
       </div>
       <div className="absolute top-0 flex gap-0.5">
-        {initialValue && (
-          <>
-            {Array.from({ length: Math.trunc(initialValue) }).map((_, i) => (
-              <Star key={i} fill="#FFC700" strokeWidth={0} size={size} />
-            ))}
-            {Math.round(initialValue) !== initialValue && (
-              <StarHalf fill="#FFC700" strokeWidth={0} size={size} />
-            )}
-          </>
+        {Array.from({ length: Math.trunc(value) }).map((_, i) => (
+          <Star key={i} fill={COLORS.point.star} strokeWidth={0} size={size} />
+        ))}
+        {Math.round(value) !== value && (
+          <StarHalf fill={COLORS.point.star} strokeWidth={0} size={size} />
         )}
       </div>
     </div>
