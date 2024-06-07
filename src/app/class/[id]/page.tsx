f36de70cb4829924574dfa59e11f8efce3098a00
Hailey Kim <haileydeveloper@gmail.com>
2024-06-07 21:14:09 +0900

    feat: make BottomBar reusable

--- src/app/class/[id]/page.tsx
+++ src/app/class/[id]/page.tsx
@@ -5,14 +5,17 @@ import ClassDetailCarousel from "@/components/classDetail/carousel/Carousel";
 import ClassDetailSummary from "@/components/classDetail/summary/Summary";
 import ClassDetailTab from "@/components/classDetail/nav/Tab";
 import ClassDetailSection from "@/components/classDetail/section/Section";
-import ClassDetailBottomActionBar from "@/components/classDetail/reservation/BottomActionBar";
+
+import BottomActionBar from "@/components/classDetail/reservation/BottomActionBar";
 import ShareModal from "@/components/classDetail/share/Modal";
+import ReservationModal from "@/components/classDetail/reservation/Modal";
 
 import { TABS } from "@/constants/classDetailTabs";
 import {
   mockClassImages,
   mockClassSectionData,
   mockClassSummaryData,
+  mockLessonData,
 } from "@/lib/mock";
 import { useClassData } from "@/hooks/useClassData";
 
@@ -39,8 +42,9 @@ export default function ClassDetailPage({ params }: Props) {
           data={mockClassSectionData[i]}
         />
       ))}
-      <ClassDetailBottomActionBar price={40000} />
+      <BottomActionBar price={40000} />
       <ShareModal />
+      <ReservationModal data={mockLessonData} price={40000} />
     </>
   );
 }
--- src/components/classDetail/reservation/BottomActionBar.tsx
+++ src/components/classDetail/reservation/BottomActionBar.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+
 interface Props {
   price: number;
+  onCheckout?: boolean;
 }
 
-export default function ClassDetailBottomActionBar({ price }: Props) {
+export default function BottomActionBar({ price, onCheckout = false }: Props) {
   const openReservationModal = () => {
     const modal = document.getElementById("reservation-modal");
     modal?.classList.remove("hidden");
@@ -15,10 +18,13 @@ export default function ClassDetailBottomActionBar({ price }: Props) {
       <div className="flex items-center justify-end gap-10 max-w-5xl h-full mx-auto">
         <span className="font-bold text-xl text-black">{`${price.toLocaleString()}원`}</span>
         <button
-          className="w-52 py-2.5 rounded font-bold text-lg text-white bg-primary"
+          className={cn(
+            "w-52 py-2.5 rounded font-bold text-lg text-white",
+            onCheckout ? "bg-[#FEE500]" : "bg-primary",
+          )}
           onClick={openReservationModal}
         >
-          {"예약하기"}
+          {onCheckout ? "카카오페이 결제" : "예약하기"}
         </button>
       </div>
     </div>
