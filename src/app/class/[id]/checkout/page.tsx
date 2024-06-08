95d319bc411eb2c6c2a42032ebef1f8aa9a6ad56
Hailey Kim <haileydeveloper@gmail.com>
2024-06-08 10:42:04 +0900

    feat: update BottomActionBar

--- src/app/class/[id]/checkout/page.tsx
+++ src/app/class/[id]/checkout/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+
+import { useRecoilValue } from "recoil";
+import { checkoutState } from "@/state/checkout";
+import BottomActionBar from "@/components/classDetail/reservation/BottomActionBar";
+
 export default function CheckoutPage() {
-  return <></>;
+  const checkout = useRecoilValue(checkoutState);
+
+  const handlePayment = () => {};
+
+  return (
+    <>
+      <h2 className="mt-16 font-bold text-2xl text-black">{"클래스 결제"}</h2>
+      <hr className="w-full my-12 border-gray" />
+      <div></div>
+      <hr className="w-full my-12 border-gray" />
+      <div></div>
+      <BottomActionBar
+        price={checkout.price}
+        onClick={handlePayment}
+        onCheckout
+      />
+    </>
+  );
 }
