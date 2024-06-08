320c815055ab97031f518888f6fb7308c60f3789
Hailey Kim <haileydeveloper@gmail.com>
2024-06-08 10:01:47 +0900

    feat: update checkoutState (add image)

--- src/lib/mock.ts
+++ src/lib/mock.ts
@@ -26,12 +26,12 @@ export const mockClassImages: ClassImage[] = [
   {
     id: 392399,
     name: "image-2",
-    url: "https://source.unsplash.com/600x400/?food",
+    url: "https://source.unsplash.com/600x400/?travel",
   },
   {
     id: 3923899,
     name: "image-3",
-    url: "https://source.unsplash.com/600x800/?work",
+    url: "https://source.unsplash.com/600x800/?travel",
   },
 ];
 
@@ -151,4 +151,5 @@ export const mockCheckoutData: CheckoutClassDetail = {
   title: mockClassSummaryData.title,
   tutor: mockClassSectionData[1].title!,
   address: mockClassSummaryData.address,
+  image: mockClassImages[0].url,
 };
