57c96932a0d3c8653be042ada23b40c89faadf48
Hailey Kim <haileydeveloper@gmail.com>
2024-06-15 19:06:56 +0900

    feat: add alert on ReservationModal, update alert and backdrop

--- src/components/common/Backdrop.tsx
+++ src/components/common/Backdrop.tsx
@@ -5,17 +5,22 @@ import { closeModal, cn } from "@/lib/utils";
 interface Props {
   children: React.ReactNode;
   className?: string;
+  backdropClassName?: string;
   onClick?: () => void;
 }
 
 export default function Backdrop({
   children,
   className,
+  backdropClassName,
   onClick = closeModal,
 }: Props) {
   return (
     <>
-      <div className="fixed inset-0 z-30 bg-black-blur" onClick={onClick} />
+      <div
+        className={cn("fixed inset-0 z-30 bg-black-blur", backdropClassName)}
+        onClick={onClick}
+      />
       <div
         className={cn(
           "fixed bottom-2/4 right-2/4 translate-x-2/4 translate-y-2/4 z-50 rounded bg-white",
