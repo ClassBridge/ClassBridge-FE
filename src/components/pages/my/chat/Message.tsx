241c7cef13b497609ea76ac444f0b83c4329b0df
Hailey Kim <haileydeveloper@gmail.com>
2024-06-20 10:14:54 +0900

    feat: update ChatMessage UI

--- src/components/pages/my/chat/Message.tsx
+++ src/components/pages/my/chat/Message.tsx
@@ -16,29 +16,23 @@ export default function Message({ data }: Props) {
   return (
     <div
       className={cn(
-        "relative flex items-end gap-1 my-6 mx-4",
+        "relative flex items-end gap-2 my-6 mx-4",
         data.me ? "flex-row-reverse" : "flex-row",
       )}
     >
       <div
         className={cn(
           "w-fit max-w-96 py-3 px-4 rounded border-primary-light",
-          data.me ? "bg-primary-blur" : "border bg-white",
+          data.me
+            ? "rounded-br-[0] bg-primary-blur"
+            : "rounded-bl-[0] border bg-white",
         )}
       >
         {data.message}
       </div>
-      <span
-        className={cn(
-          "font-normal text-sm text-black",
-          data.me ? "mr-2" : "ml-2",
-        )}
-      >
-        {data.time}
-      </span>
-      <span className="font-normal text-sm text-black">{"•"}</span>
-      <span className="font-normal text-sm text-black">
-        {data.read ? "읽음" : "안읽음"}
+      <span className="font-normal text-sm text-black">{data.time}</span>
+      <span className="font-normal text-sm text-primary">
+        {!data.read && 1}
       </span>
     </div>
   );
