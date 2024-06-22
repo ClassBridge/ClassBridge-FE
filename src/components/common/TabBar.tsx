717479b145ccd3d648b2b4342701ead24bb86e73
Hailey Kim <haileydeveloper@gmail.com>
2024-06-22 17:19:10 +0900

    feat: create TabBar component, MyPage Class layout

---
+++ src/components/common/TabBar.tsx
@@ -0,0 +1,27 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+interface Props {
+  tabs: {
+    values: string[];
+    contents: string[];
+  };
+}
+
+export default function TabBar({ tabs }: Props) {
+  return (
+    <Tabs defaultValue={tabs.values[0]} className="w-[790px]">
+      <TabsList>
+        {tabs.values.map((tab) => (
+          <TabsTrigger key={tab} value={tab}>
+            {tab}
+          </TabsTrigger>
+        ))}
+      </TabsList>
+      {tabs.contents.map((content, i) => (
+        <TabsContent key={i} value={tabs.values[i]}>
+          {content}
+        </TabsContent>
+      ))}
+    </Tabs>
+  );
+}
