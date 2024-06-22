da14484f0040ddd94df06a346888035e3d1ab720
Hailey Kim <haileydeveloper@gmail.com>
2024-06-22 14:13:07 +0900

    feat: create mypage like, likes table action and hook

--- src/constants/supabase.ts
+++ src/constants/supabase.ts
@@ -5,6 +5,7 @@ export const LESSON_TABLE = "lesson";
 export const RESERVATION_TABLE = "reservation";
 export const CHATROOM_TABLE = "chatroom";
 export const CHAT_TABLE = "chat";
+export const LIKES_TABLE = "likes";
 
 export const PROFILE_BUCKET = "profile";
 export const CLASS_BUCKET = "class";
