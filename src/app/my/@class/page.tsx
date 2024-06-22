a892289f3b72abe3f7f6bde4cb4c429c7e398159
Hailey Kim <haileydeveloper@gmail.com>
2024-06-22 17:19:10 +0900

    feat: create TabBar component, MyPage Class layout

--- src/app/my/@class/page.tsx
+++ src/app/my/@class/page.tsx
@@ -1,9 +1,21 @@
+import TabBar from "@/components/common/TabBar";
 import NoContent from "@/components/pages/my/NoContent";
 
 export default function ClassPage() {
+  const tabs = {
+    values: ["전체", "수강 완료", "예약 확정", "예약 취소"],
+    contents: ["", "", "", ""],
+  };
+
   return (
     <>
-      <NoContent name="예약한 클래스가" />
+      {tabs ? (
+        <section className="flex-1 flex justify-center py-6">
+          <TabBar tabs={tabs} />
+        </section>
+      ) : (
+        <NoContent name="예약한 클래스가" />
+      )}
     </>
   );
 }
