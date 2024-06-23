e1b8001c4d1171712411f01058a9a97331135996
Hailey Kim <haileydeveloper@gmail.com>
2024-06-23 15:45:15 +0900

    feat: update TabBar TabsContent

--- src/components/common/TabBar.tsx
+++ src/components/common/TabBar.tsx
@@ -1,27 +1,21 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 interface Props {
-  tabs: {
-    values: string[];
-    contents: React.ReactNode[];
-  };
+  tabs: readonly string[];
+  children: React.ReactNode;
 }
 
-export default function TabBar({ tabs }: Props) {
+export default function TabBar({ tabs, children }: Props) {
   return (
-    <Tabs defaultValue={tabs.values[0]} className="w-[790px]">
+    <Tabs defaultValue={tabs[0]} className="w-[790px]">
       <TabsList>
-        {tabs.values.map((tab) => (
+        {tabs.map((tab) => (
           <TabsTrigger key={tab} value={tab}>
             {tab}
           </TabsTrigger>
         ))}
       </TabsList>
-      {tabs.contents.map((content, i) => (
-        <TabsContent key={i} value={tabs.values[i]}>
-          {content}
-        </TabsContent>
-      ))}
+      {children}
     </Tabs>
   );
 }
