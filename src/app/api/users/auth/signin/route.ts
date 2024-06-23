5ef9b3da5f5db5b863620437e8774310b1eff9c5
Hailey Kim <haileydeveloper@gmail.com>
2024-06-23 16:49:22 +0900

    feat: update routes to use env variable

--- src/app/api/users/auth/signin/route.ts
+++ src/app/api/users/auth/signin/route.ts
@@ -4,7 +4,7 @@ export const POST = async (request: NextRequest) => {
   const data = await request.json();
 
   const response = await fetch(
-    "http://13.125.180.170:8080/api/users/auth/signin",
+    `${process.env.ALLOWED_ORIGIN}/api/users/auth/signin`,
     {
       method: "POST",
       headers: {
