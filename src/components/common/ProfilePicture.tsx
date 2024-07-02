4ee39c0df9e77ccb78b42202b81fbf019e660a97
Hailey Kim <haileydeveloper@gmail.com>
2024-07-02 14:42:11 +0900

    chore: add extraLarge option on ProfilePicture

--- src/components/common/ProfilePicture.tsx
+++ src/components/common/ProfilePicture.tsx
@@ -2,24 +2,27 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { cn } from "@/lib/utils";
 
 interface Props {
-  src?: string;
+  src?: string | null;
   fallback: string;
   large?: boolean;
+  extraLarge?: boolean;
 }
 
 export default function ProfilePicture({
   src,
   fallback,
   large = false,
+  extraLarge = false,
 }: Props) {
   return (
     <Avatar
       className={cn(
         "bg-point shadow-[0_0_4px_0_rgba(0,0,0,0.2)]",
-        large && "w-[50px] h-[50px] ",
+        large && "w-[50px] h-[50px]",
+        extraLarge && "w-[70px] h-[70px]",
       )}
     >
-      <AvatarImage src={src} alt={fallback} />
+      <AvatarImage src={src || ""} alt={fallback} />
       <AvatarFallback className="font-bold text-white">
         {fallback.slice(0, 2).toUpperCase()}
       </AvatarFallback>
